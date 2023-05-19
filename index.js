610b8a9f6e3d8eb022f577653a085ff27ea24763
silva guimaraes <silva__guimaraes@hotmail.com>
2023-05-19 01:18:02 -0300

    retornar wave

--- index.js
+++ index.js
@@ -4,8 +4,16 @@ canvas.width = canvas.getBoundingClientRect().width / 4;
 
 var ctx = canvas.getContext("2d");
 var canvas_data = ctx.getImageData(0, 0, canvas.width, canvas.height);
+var time = 0;
+const tube_size = canvas.width * canvas.height;
+const update_interval = 10; // milliseconds
+const line_rate = 40;
+let speed_slider = document.getElementById("speed");
+let speed = 2500;
+// speed_slider.oninput = function () { // fixme: line feed fica todo preto
+//     speed = this.value
+// }
 
-// That's how you define the value of a pixel
 function drawPixel (x, y, r, g, b, a) {
     var timeex = (x + y * canvas.width) * 4;
 
@@ -15,8 +23,6 @@ function drawPixel (x, y, r, g, b, a) {
     canvas_data.data[timeex + 3] += a;
 }
 
-// That's how you update the canvas, so that your
-// modification are taken in consideration
 function update_canvas() {
     ctx.putImageData(canvas_data, 0, 0);
 }
@@ -45,15 +51,18 @@ const GREEN = 1;
 const BLUE = 2;
 const ALPHA = 3;
 
-function sine_wave(arr, time, amount, channel, wavelen, phase) {
+function sine_wave(amount, channel, wavelen, phase) {
     wavelen = 1 / wavelen;
+    let index = time;
+    let temp_array = new Uint8ClampedArray(amount);
+
     for (let i = 0; i < amount; i += 4) {
-        const sine_result = Math.sin(time++ * wavelen + phase);
+        const sine_result = Math.sin(index++ * wavelen + phase);
         const normalized_value = Math.round(((sine_result + 1) / 2) * 255);
-        arr[i + channel] += normalized_value;
-        arr[i + ALPHA] = 255;
+        temp_array[i + channel] += normalized_value;
+        temp_array[i + ALPHA] = 255;
     }
-    return time;
+    return temp_array;
 }
 
 function triangle_wave(arr, channel, wavelength, phase) {
@@ -75,53 +84,57 @@ function square_wave(arr, channel, n) {
     }
 }
 
-function shift_elements_right(arr, n) {
-    const len = arr.length;
-    const shift_amount = (n * 4) % len;
-
+function shift_elements_right(shift_amount, output) {
+    const len = canvas_data.data.length;
     if (shift_amount === 0) {
         return;
     }
 
     // Create a temporary array to store the shifted elements
-    const tempArray = new Uint8ClampedArray(shift_amount);
+    const temp_array = new Uint8ClampedArray(shift_amount);
     for (let i = 0; i < shift_amount; i++) {
-        tempArray[i] = arr[len - shift_amount + i];
+        temp_array[i] = canvas_data.data[len - shift_amount + i];
     }
 
     // Shift the remaining elements to the right
     for (let i = len - 1; i >= shift_amount; i--) {
-        arr[i] = arr[i - shift_amount];
+        canvas_data.data[i] = canvas_data.data[i - shift_amount];
     }
 
-    for (let i = 0; i < shift_amount; i++) arr[i] = 0;
-    return shift_amount
+    for (let i = 0; i < shift_amount; i++) 
+        canvas_data.data[i] = output[i];
 }
 
 
-const tube_size = canvas.width * canvas.height;
-const update_interval = 10; // milliseconds
-const line_rate = 40;
 
-let speed_slider = document.getElementById("speed");
-let speed = 2500;
-// speed_slider.oninput = function () { // fixme: line feed fica todo preto
-//     speed = this.value
-// }
+function add(a, b) {
 
+    if (a.length != b.length)
+        throw new Error("a.length != b.length");
+
+    let ret = new Uint8ClampedArray(a.length);
+    for (let i = 0; i < ret.length; i++) {
+        ret[i] = a[i] + b[i];
+    }
+    return ret;
+}
 
 function start() {
     blank();
-    let time = 0;
 
     return setInterval(() => {
-        let deficit = shift_elements_right(canvas_data.data, (tube_size * line_rate) / update_interval + speed);
-        // console.log(deficit);
-        sine_wave(canvas_data.data, time, deficit, RED,     250000,    1);
-        sine_wave(canvas_data.data, time, deficit, GREEN,   250000,    2);
-        sine_wave(canvas_data.data, time, deficit, BLUE,    250000,    3);
+        const len = canvas_data.data.length;
+        const n = (tube_size * line_rate) / update_interval + speed;
+        const shift_amount = (n * 4) % len;
 
-        time += deficit / 4; // overflow!
+        let red = sine_wave(shift_amount, RED,      250000,     1);
+        let green = sine_wave(shift_amount, GREEN,  250000,     2);
+        let blue = sine_wave(shift_amount, BLUE,   250000,      3);
+
+        shift_elements_right(shift_amount, add(red, add(green, blue)));
+
+        time += shift_amount / 4; // overflow!
+            console.log(time);
         update_canvas();
     }, update_interval);
 }
